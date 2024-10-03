56e9d1b0db93d4a420be034921ad0d1a73cd4f1b
wigarde <f.wigarde@gmail.com>
2024-10-03 09:36:16 +0200

    Initial commit

---
+++ Devoteam-Robot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Devoteam_Robot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
