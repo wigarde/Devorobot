2214af15313d2a7350967e8d7e9783a3c7d8f315
wigarde <f.wigarde@gmail.com>
2024-10-03 10:15:13 +0200

    Update Program.cs
    Fixed and changed some inputs also added a try / catch

--- Devoteam-Robot/Program.cs
+++ Devoteam-Robot/Program.cs
@@ -1,24 +1,30 @@
 ﻿using Devoteam_Robot;
 using System;
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Enter room size as 'width height'");
-        string roomSizeInput = Console.ReadLine();
+        try
+        {
+            Console.WriteLine("Enter room size as 'width height'");
+            string roomSizeInput = Console.ReadLine();
 
-        string[] roomSizes = roomSizeInput.Split();
-        int width = int.Parse(roomSizes[0]);
-        int height = int.Parse(roomSizes[1]);
+            string[] roomSizes = roomSizeInput.Split();
+            int width = int.Parse(roomSizes[0]);
+            int height = int.Parse(roomSizes[1]);
 
-        Console.WriteLine("Enter robot starting position and direction as '23N'");
-        string positionInput = Console.ReadLine();
-        string[] robotPosition = positionInput.Split();
-        int x = int.Parse(robotPosition[0]);
-        int y = int.Parse(robotPosition[1]);
-        char direction = char.Parse(robotPosition[2]);
+            Console.WriteLine("Enter robot starting position and direction as '23N'");
+            char[] positionInput = Console.ReadLine().ToCharArray();
+            int x = int.Parse(positionInput[0].ToString());
+            int y = int.Parse(positionInput[1].ToString());
+            char direction = positionInput[2];
 
 
-        Robot robot = new Robot(width, height, x, y, direction);
+            Robot robot = new Robot(width, height, x, y, direction);
+        }
+
+        catch (Exception ex) { 
+            Console.WriteLine(ex.Message);
+        }
     }
 }
